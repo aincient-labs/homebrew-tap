4cc0198774dfce053cdaaf3e0ada0444fbaf6f6d
Starla Knight <269940154+aincient-labs@users.noreply.github.com>
2026-07-19 00:17:06 +0000

    atelier 0.2.6

--- Formula/atelier.rb
+++ Formula/atelier.rb
@@ -1,25 +1,25 @@
 class Atelier < Formula
   desc "The `atelier` command — install, update, back up, and manage your Atelier CMS appliance."
   homepage "https://github.com/aincient-labs/manager"
-  version "0.2.5"
+  version "0.2.6"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/aincient-labs/manager/releases/download/v0.2.5/atelier-aarch64-apple-darwin.tar.xz"
-      sha256 "e40b1d530ca5dd223c7559840045711e5fc9718a63521dea3995300568c57c2d"
+      url "https://github.com/aincient-labs/manager/releases/download/v0.2.6/atelier-aarch64-apple-darwin.tar.xz"
+      sha256 "20d50bf673499633d6a122e3209f4f76181ab7c3a5043f2c1933395d98f23883"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/aincient-labs/manager/releases/download/v0.2.5/atelier-x86_64-apple-darwin.tar.xz"
-      sha256 "494c04837ab330aa11efac8b4dfc59cebdf32d021a32a6f2756562091739bfa5"
+      url "https://github.com/aincient-labs/manager/releases/download/v0.2.6/atelier-x86_64-apple-darwin.tar.xz"
+      sha256 "dd200756017e668e3a9075d99bb7cc025d92a83e4a82dd687d8019f2aba3e42d"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/aincient-labs/manager/releases/download/v0.2.5/atelier-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "a7adb281f93bbad85713fcf38c0b15c2181774264f4085d80b8896ebc7668ee7"
+      url "https://github.com/aincient-labs/manager/releases/download/v0.2.6/atelier-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "87bdebf2a8152e032d54208efb5077f50488e4a6c51be79a1ae71396e3b45daa"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/aincient-labs/manager/releases/download/v0.2.5/atelier-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "2302906d8e490b0a33c0b40bf7039b9c56f231f37426e8de2466cc203493e5f1"
+      url "https://github.com/aincient-labs/manager/releases/download/v0.2.6/atelier-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "bbb02d665ffa7bc8fa288018f67a8b7e0bfd69a62848b9c161bbcf6711893ca3"
     end
   end
   license "GPL-2.0-or-later"
